482c3ea1eeff5d9ed77b29ea78f674248aee5257
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2022-09-22 01:17:36 +0300

    лабораторная работа 2

---
+++ Задание_3.py
@@ -0,0 +1,11 @@
+#biggest of three 
+a = int(input("введите первое число: "))
+b = int(input("введите второе число: "))
+c = int(input("введите третье число: ")) 
+
+d = max(a,max(b,c))
+print(f"наибольшее => {d}")
+
+
+
+
---
+++ Задание_4.py
@@ -0,0 +1,13 @@
+#number of unique numbers
+z = []
+for i in range(5):
+    a = int(input("введите число: "))
+    z.append(a)
+
+z = set(z)
+print(f"количество уникальных номеров равно {len(z)}")
+
+
+
+
+
