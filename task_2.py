1df7101c38dc369037531f73a02536c51bafb592
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2022-10-02 20:42:07 +0300

    lab 3

---
+++ task_2.py
@@ -0,0 +1,15 @@
+def unique():
+    lst = []
+    for i in range(10):
+        num = eval(input("введите число: "))
+        lst.append(num)
+    return len([i for i in lst if i == 0])
+
+
+
+
+
+
+
+
+print(f"количество чисел = 0 равно {unique()}")
