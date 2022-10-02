2a98a42879d838640af478524009941243554add
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2022-10-02 21:57:34 +0300

    lab 3

---
+++ lab-3/task_1.py
@@ -0,0 +1,19 @@
+
+def total10():
+    lst = []
+    for i in range(10):
+        num = eval(input("введите число: "))
+        lst.append(num)
+
+    return sum(lst)
+
+print(f"сумма чисел = {total10()}")    
+
+
+
+
+
+
+    
+
+
