a1bfa01f0439434a0ffd61b0f4d9cfab6d7b707c
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2022-10-15 11:33:26 +0300

    task 4

--- lab-3/task_4.py
+++ lab-3/task_4.py
@@ -1,10 +1,18 @@
+
 n = int(input("введите натуральное число: "))
 
+
 for i in range(1,n+1):
-    for x in range(1,i+1):
-        print(x,end='') 
-    #обратный
-    for k in range(i-1,0,-1):
+    #spaces in the beggining
+    for j in range(n-i,0,-1):
+        print(" ",end="")
+        
+    for k in range(1,i+1):
         print(k,end='')
+
+    #обратный
+    for t in range(i-1,0,-1):
+        print(t,end='')
+    #go to the next line 
     print()
-    
+        
